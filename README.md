9c38c47797684efa33f1aeccbfca152ba9809f54
Noi Sek <noi.t.sek@gmail.com>
2015-03-26 20:16:15 -0700

    Re-written. Prevents queued sounds, improves performance, fixes several latent issues

--- README.md
+++ README.md
@@ -3,17 +3,25 @@ Sound Alert
 A plugin that plays a random sound whenever you receive an alert.
 Optional TF2 Taunts available [here](http://github.com/captain-lightning/Sound-Alert). 
 
-### Installation
-- To install, move soundoff.py into %appdata%/HexChat/addons for Windows, ~/.config/HexChat/addons for linux. Once there, it will load automatically.
-- Move alert sounds into share/sounds within Hexchat's install folder or specify your own sounds folder with "/soundalert set <path>", see Usage.
-
 ### Requirements
-- Python scripting interface plugin.
+- Python scripting interface plugin for Hexchat.
 - Xine, for linux users.
+
+### Installation
+
+##### Windows
+- To install, move soundalert.py into ```%appdata%\HexChat\addons```. Once there, it will load automatically.
+- Move alert sounds into share/sounds within Hexchat's install directory, ```%appdata\HexChat\sounds```. Alternatively, specify your own sounds folder with "/soundalert set <path>", see: [Usage](#Usage).
+
+##### Linux
+- Install Xine. On Ubuntu: ```apt-get install xine```
+- Install PyXine with ```pip install pyxine```
+- To install, move soundalert.py into ```~/.config/HexChat/addons```. Once there, it will load automatically.
+- Move alert sounds into share/sounds within Hexchat's install directory, ```/usr/local/bin/HexChat/share/sounds```. Alternatively, specify your own sounds folder with "/soundalert set <path>", see: [Usage](#Usage).
 
 ### Features
 - Plays all formats Xine does in Linux. (AAC, AC3, ALAC, AMR, FLAC, MP3, RealAudio, Shorten, Speex, Vorbis, WMA)
-- Plays only .wav in Windows.
+- Plays only .wav in Windows. Unfortunately adding support for more formats would involve compiling and installing many dependencies from source. Savvy users using 2.7 or older may try using [PyMedia](http://www.lfd.uci.edu/~gohlke/pythonlibs/#pymedia).
 - Automatically searches for and plays from the share/sounds folder within Hexchat's install directory by default.
 
 ### Usage
--- soundalert.py
+++ soundalert.py
@@ -2,104 +2,123 @@ import hexchat, sys, os, random
 from threading import Thread
 
 __module_name__ = "Sound Alert" 
-__module_version__ = "3.2"
+__module_version__ = "4.0"
 __module_description__ = "Plays a random sound on alert from Hexchat/share/sounds \
 by default or the directory specified by \"/soundalert set my_sounds/directory\""
 
+# Winsound is installed by default
+if os.name == "nt":
+  import winsound
 
-hexchat.prnt("Sound Alert plugin loaded.")
+# Pyxine is a third-party module which must be installed
+elif os.name =="posix":
+  try:
+    import pyxine
 
-if hexchat.get_pluginpref("soundalert_active") == False:
-  hexchat.prnt("Alerts are currently disabled. Re-enable them with /alertson")
+  except ImportError:
+    print("Pyxine is missing! Install xine, then run 'pip install pyxine' to use this plugin.")
+    raise
 
-soundalert_active = True
+else:
+  raise Exception("This operating system is not supported.")
 
-def alertsoff(word, word_eol, userdata):
-  hexchat.prnt("Sound alerts will now be off until you enable them again with /alertson.")
-  hexchat.set_pluginpref("soundalert_active", False)
+class SoundAlert():
+  def __init__(self):
+    hexchat.prnt("Sound Alert plugin loaded.")
 
-def alertson(word, word_eol, userdata):
-  hexchat.prnt("Sound alerts are now on.")
-  hexchat.set_pluginpref("soundalert_active", True)
+    self.sound_directory = self.find_sound_directory()
+    self.file_list = self.find_sounds()
 
-def parse(word, word_eol, userdata):
-  if len(word) < 3:
-    hexchat.prnt("Not enough arguments given. See /help soundalert")
+    if hexchat.get_pluginpref("soundalert_active") == None:
+      hexchat.set_pluginpref("soundalert_active", True)
 
-  else:
-    if word[1] == "set":
-      if os.path.isdir(word_eol[1]):
-        hexchat.set_pluginpref("soundalert_dir", word_eol[1])
+    if hexchat.get_pluginpref("soundalert_active") == False:
+      hexchat.prnt("Alerts are currently disabled. Re-enable them with /alertson")
 
-      else:
-        hexchat.prnt("Not a valid directory.")
+  def disable(self, word, word_eol, userdata):
+    hexchat.prnt("Sound alerts will now be off until you enable them again with /alertson.")
+    hexchat.set_pluginpref("soundalert_active", False)
 
-  return hexchat.EAT_ALL
+  def enable(self, word, word_eol, userdata):
+    hexchat.prnt("Sound alerts are now on.")
+    hexchat.set_pluginpref("soundalert_active", True)
 
-def find_sounds():
-  if hexchat.get_pluginpref("soundalert_dir") != None:
-    return hexchat.get_pluginpref("soundalert_dir")
-
-  else:
-    if os.name == "nt":
-      paths = ["C:\Program Files\HexChat\share\sounds", "C:\Program Files (x86)\HexChat\share\sounds"]
-
-    elif os.name == "posix":
-      paths = ["/sbin/HexChat/share/sounds", "/usr/sbin/HexChat/share/sounds", "/usr/local/bin/HexChat/share/sounds"]
+  def find_sound_directory(self):
+    if hexchat.get_pluginpref("soundalert_dir") != None:
+      return hexchat.get_pluginpref("soundalert_dir")
 
     else:
+      if os.name == "nt":
+        paths = ["C:\Program Files\HexChat\share\sounds", "C:\Program Files (x86)\HexChat\share\sounds", "%appdata%\HexChat\sounds"]
+
+      elif os.name == "posix":
+        paths = ["/sbin/HexChat/share/sounds", "/usr/sbin/HexChat/share/sounds", "/usr/local/bin/HexChat/share/sounds"]
+
+      else:
+        return False
+
+      for path in paths:
+        if os.path.isdir(path):
+          hexchat.set_pluginpref("soundalert_dir", path)
+          return path
+
       return False
 
-    for path in paths:
-      if os.path.isdir(path):
-        hexchat.set_pluginpref("soundalert_dir", path)
-        return path
+  def find_sounds(self):
+    if self.sound_directory == False:
+      return False
 
-    return False
+    os.chdir(self.sound_directory)
+    file_list = list()
 
+    for root, dirs, files in os.walk("./"):
+      for sound_file in files:
+        file_list.append(sound_file)
+    
+    return file_list
 
-def process(word, word_eol, userdata):
-  do_thread = Thread(target=play_sound)
-  do_thread.start()
+  def play_sound(self):
+    sound = random.choice(self.file_list)
+    active = hexchat.get_pluginpref("soundalert_active")
 
-def randomize():
-  sound_dir = find_sounds()
-  if sound_dir == False:
-    return False
+    if not active:
+      return False
 
-  os.chdir(sound_dir)
-  file_list = list()
+    if sound == False:
+      hexchat.prnt("Could not find default share/sounds directory, and no sounds directory is specified. See /help soundalert.")
 
-  for file in os.listdir("./"):
-    file_list.append(file)
-  
-  random.shuffle(file_list)
-  return file_list[0]
+    if os.name == "nt":
+      winsound.PlaySound(sound, winsound.SND_FILENAME ^ winsound.SND_ASYNC)
 
-def play_sound():
-  sound = randomize()
-  active = hexchat.get_pluginpref("soundalert_active")
+    elif os.name == "posix":
+      xine = pyxine.Xine()
+      stream = xine.stream_new()
+      stream.open(sound)
+      stream.Play()
 
-  if not active:
-    return False
+  def set_options(self, word, word_eol, userdata):
+    if len(word) < 3:
+      hexchat.prnt("Not enough arguments given. See /help soundalert")
 
-  if sound == False:
-    hexchat.prnt("Could not find default share/sounds directory, and no sounds directory is specified. See /help soundalert.")
+    else:
+      if word[1] == "set":
+        if os.path.isdir(word_eol[1]):
+          hexchat.set_pluginpref("soundalert_dir", word_eol[1])
 
-  if os.name == "nt":
-    import winsound
-    winsound.PlaySound('%s' % sound, winsound.SND_FILENAME)
+        else:
+          hexchat.prnt("Not a valid directory.")
 
-  elif os.name == "posix":
-    import pyxine
-    xine = pyxine.Xine()
-    stream = xine.stream_new()
-    stream.open(sound)
-    stream.Play()
+    return hexchat.EAT_ALL
 
-hexchat.hook_command("soundalert", parse, help="/soundalert set <directory> -- Sets a directory for Sound Alert to pull sounds from.")
-hexchat.hook_command("alertson", alertson, help="Turns on soundalert alerts.")
-hexchat.hook_command("alertsoff", alertsoff, help="Turns off soundalert alerts.")
-hexchat.hook_print("Channel Action Hilight", process)
-hexchat.hook_print("Channel Msg Hilight", process)
-hexchat.hook_print("Private Message", process)
+  def spawn(self, word, word_eol, userdata):
+    do_thread = Thread(target=self.play_sound)
+    do_thread.start()
+
+alert = SoundAlert()
+
+hexchat.hook_command("soundalert", alert.set_options, help="/soundalert set <directory> -- Sets a directory for Sound Alert to pull sounds from.")
+hexchat.hook_command("alertson", alert.enable, help="Turns on soundalert alerts.")
+hexchat.hook_command("alertsoff", alert.disable, help="Turns off soundalert alerts.")
+hexchat.hook_print("Channel Action Hilight", alert.spawn)
+hexchat.hook_print("Channel Msg Hilight", alert.spawn)
+hexchat.hook_print("Private Message", alert.spawn)
